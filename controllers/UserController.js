dcb547acd4bfa38a13b5fe75fd40cb1908c79048
mario <daw@grupapunts.es>
2024-11-20 17:13:23 +0100

    delete creado

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,31 +1,51 @@
 const { User,Order } = require("../models/index")//importar modelo
-
+const bcrypt = require("bcryptjs");
 const UserController = {
-    async create(req,res){
+    async create(req, res) {
         try {
-            // console.log(req.body)
-            const user = await User.create(req.body)
-        res.status(201).send({message:"Usuario creado",user})
+          // console.log(req.body)
+          req.body.password = await bcrypt.hash(req.body.password, 10);
+          const user = await User.create(req.body);
+          res.status(201).send({ message: "Usuario creado", user });
         } catch (error) {
-            console.error(error);
-            res.status(500).send({message:"Ha habido un error",error})
+          console.error(error);
+          res.status(500).send({ message: "There was a problem", error });
         }
-    },
-    async getAll(req,res){
+      },
+      async getAll(req, res) {
         try {
-            const users = await User.findAll({
-                // include:[Order]
-                include:{
-                    model:Order,
-                    attributes:["date","userId"]
-                }
-    
-            })
-            res.status(200).send(users)
+          const users = await User.findAll({
+            // include:[Order]
+            include: {
+              model: Order,
+              attributes: ["date", "UserId"],
+            },
+          });
+          res.status(200).send(users);
         } catch (error) {
-            console.error(error);
-            res.status(500).send({message:"Ha habido un error",error})
+          console.error(error);
+          res.status(500).send({ message: "There was a problem", error });
         }
-      }
+      },
+      async delete(req, res) {
+        try {
+          //eliminar las publicaciones del usuario
+          await Order.destroy({
+            where: {
+              UserId: req.params.id,
+            },
+          });
+          //eliminar el usuario
+          await User.destroy({
+            where: {
+              id: req.params.id,
+            },
+          });
+          res.send({ message: `User with id ${req.params.id} deleted` });
+        } catch (error) {
+          console.error(error);
+          res.status(500).send({ message: "There was a problem", error });
+        }
+      },
 };
 module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,5 @@ const UserController = require("../controllers/UserController")
 const router = express.Router()
 router.post("/create",UserController.create)
 router.get("/getAll",UserController.getAll)
+router.delete("/id/:id",UserController.delete)
 module.exports = router
