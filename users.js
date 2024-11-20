9dffc86b0f8d1cd1ebbfd92ce3147bb1829570b3
mario <daw@grupapunts.es>
2024-11-20 13:10:27 +0100

    users fixed

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const UserController = require("../controllers/UserController")
+const UserController = require("./controllers/UserController")
 
 const router = express.Router()
 router.post("/create",UserController .create)
