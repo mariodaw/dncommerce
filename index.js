e48c25768a26b0c05555dd6f35bf7960205ea65c
mario <daw@grupapunts.es>
2024-11-20 13:10:42 +0100

    users fixed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 app.use(express.json())//parsea body si no ponemos esto req.body es undefined
-app.use("/users",require("./users.js"))
+app.use("/users",require("./routes/users.js"))
 app.use("/posts",require("./routes/orders.js"))
 app.listen(PORT,()=> console.log("Servidor a nivel de Españita en "+PORT))
