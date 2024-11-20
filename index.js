b0380432c68dce0fdb8c1524f9ec90da2aa7d35f
mario <daw@grupapunts.es>
2024-11-20 15:39:27 +0100

    Order creado y visto

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const PORT = 3000
 app.use(express.json())//parsea body si no ponemos esto req.body es undefined
 app.use("/users",require("./routes/users.js"))
-app.use("/posts",require("./routes/orders.js"))
+app.use("/orders",require("./routes/orders.js"))
 app.listen(PORT,()=> console.log("Servidor a nivel de Españita en "+PORT))
