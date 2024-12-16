fac759f6303e98ab7eeb98514d8d635227fbf183
mario <daw@grupapunts.es>
2024-12-16 13:04:40 +0100

    cors instalado

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 const { typeError } = require('./middleware/errors');
+const cors = require("cors")
 
 app.use(express.json())//parsea body si no ponemos esto req.body es undefined
 app.use("/users",require("./routes/users.js"))
@@ -9,4 +10,5 @@ app.use("/orders",require("./routes/orders.js"))
 app.use("/categories",require("./routes/categories.js"))
 app.use("/products",require("./routes/products.js"))
 app.use(typeError)
+app.use(cors())
 app.listen(PORT,()=> console.log("Servidor a nivel de Españita en "+PORT))
